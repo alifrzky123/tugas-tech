9ef2c62bfc96cd6f0ccba4b8efbcf841ba007138
alifrizky <alifrzky78@gmail.com>
2019-12-08 13:25:22 +0700

    berubah beberapa

--- dPribadi.java
+++ dPribadi.java
@@ -1,15 +1,16 @@
 public class dPribadi{
     public static void main (String[] args){
-        Long namasaya = "Alif Rizky Excelsys Hanjono";
+        String namasaya = "Alif Rizky Excelsys Hanjono";
         String tempatlahir = "Jakarta";
-        Int tanggallahir = 29;
-        String bulan = Agustus;
-        Int tahunlahir = 1999;
-        Double umur = 2019-tahunlahir;
+        int tanggallahir = 29;
+        String bulan = "Agustus";
+        int tahunlahir = 1999;
+        int umur = 2019-tahunlahir;
+        
         String alamat = "Perumahan Taman Pondok Cabe Blok A5 No.2 Rt 1 Rw 8 Pamulang Timur, Tangerang Selatan, Banten";
         System.out.println(namasaya);
-        System.out.println(tempatlahir, " ", tanggallahir, "-", bulan, " ", tahunlahir);
-        System.out.println(umur);
-        System.out.println(alamat);
+        System.out.println(tempatlahir+ " "+ tanggallahir+ "-"+ bulan+ " "+ tahunlahir);
+        System.out.println("Umur saya adalah : "+umur+ " Tahun");
+        System.out.println("Alamat Saya Adalah : "+alamat);
     }
 }
