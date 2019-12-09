562034f6b390730bc95db9fdbe43213da0634745
alifrizky <alifrzky78@gmail.com>
2019-12-09 12:55:08 +0700

    coba logical operators

--- coba.java
+++ coba.java
@@ -1,21 +1,13 @@
 public class coba{
     public static void main(String[]args){
-        int a = 10;
-        int b = 5;
-        a += 3;
+        boolean a = true;
+        boolean b = false;
 
-        System.out.println(a); // 13
-        a = b;
-        System.out.println(b); // 5 
-        a += b;
-        System.out.println(a);// 10
-        b -= a;
-        System.out.println(b); //-5
-        b *= b;
-        System.out.println(b); //25
-        b/=a;
-        System.out.println(b); //2
-        a %= b;
-        System.out.println(a); //0
+        System.out.println(a && b); //false
+        System.out.println(a || b); //true
+        System.out.println(!a); //false
+        System.out.println(!a && b);
+        System.out.println(!(a && b));
+
     }
 }
