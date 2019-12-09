767d88ab50257a8f5b52fe0e25db87e6bd865e05
alifrizky <alifrzky78@gmail.com>
2019-12-09 11:45:19 +0700

    baik kak

---
+++ coba.java
@@ -0,0 +1,20 @@
+public class coba{
+    public static void main(String[]args){
+        double a = 7;
+        double b = 5;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println("increment"+ a++);
+        System.out.println("increment"+ a);
+        System.out.println("decrement"+ --a);
+        System.out.println(b++);
+        System.out.println(b--);
+        System.out.println((a*(a+b)/a)+b);
+
+
+    }
+}
