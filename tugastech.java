19dd01e7adf341c066c6afce700f7597fca14278
alifrizky <alifrzky78@gmail.com>
2019-12-13 14:00:24 +0700

    menambah pilihan untuk keluar

--- tugastech.java
+++ tugastech.java
@@ -50,23 +50,43 @@ public class tugastech{
                 baris ("alas = ");double a = input.nextDouble();
                 baris ("tinggi = ");double t = input.nextDouble();
                 cetak(LSegitiga(a,t));
+                int jawab = input.nextInt();
+                if ((jawab <=1) || (jawab >=3)){
+                    cetak ("anda keluar menu");
+                    System.exit(0);
+                }
                 break;
                 case 2:
                 baris ("panjang = "); double p = input.nextDouble();
                 baris ("lebar = "); double l = input.nextDouble();
                 baris ("tinggi = "); double t1 = input.nextDouble();
                 cetak(VBalok(p,l,t1));
+                int jawab1 = input.nextInt();
+                if ((jawab1 <=1) || (jawab1 >=3)){
+                    cetak ("anda keluar menu");
+                    System.exit(0);
+                }
                 break;
                 case 3:
                 baris ("panjang = "); double p1 = input.nextDouble();
                 baris ("lebar = "); double l1 = input.nextDouble();
                 baris ("tinggi = "); double t2 = input.nextDouble();
                 cetak(VPrismaSegitiga(p1,l1,t2));
+                int jawab2 = input.nextInt();
+                if ((jawab2 <=1) || (jawab2 >=3)){
+                    cetak ("anda keluar menu");
+                    System.exit(0);
+                }
                 break;
                 case 4:
                 baris ("jari-jari = "); double r = input.nextDouble();
                 baris ("tinggi = ");  double t3 = input.nextDouble();
                 cetak(VKerucut(r,t3));
+                int jawab3 = input.nextInt();
+                if ((jawab3 <=1) || (jawab3 >=3)){
+                    cetak ("anda keluar menu");
+                    System.exit(0);
+                }
                 break;
                 default:
                 System.exit(0);
