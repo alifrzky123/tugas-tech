dad0898b750c62ec2f659ab9edd06e20f70a4d88
alifrizky <alifrzky78@gmail.com>
2019-12-13 13:32:39 +0700

    menambah try and catch serta penjelasan di setiap variable

--- tugastech.java
+++ tugastech.java
@@ -4,6 +4,9 @@ public class tugastech{
     public static void cetak(Object obj){
         System.out.println(obj);
     }
+    public static void baris(Object obj){
+        System.out.print(obj);
+    }
 
     public static double LSegitiga(double a,double t){
         double hasil = (a*t)/2;
@@ -31,31 +34,46 @@ public class tugastech{
 
     }
     public static void menu(){
-        Scanner input = new Scanner (System.in);
-        cetak ("=============================");
-        cetak ("SELAMAT DATANG DI PROGRAM MTK");
-        cetak ("1. Luas Segitiga");
-        cetak ("2. Volume Balok");
-        cetak ("3. Volume Prisma Segitiga");
-        cetak ("4. Volume Kerucut");
-        cetak ("5. Keluar");
-        cetak ("Silahkan pilih yang kamu mau : ");
-        int pilih = input.nextInt();
-        switch(pilih){
-            case 1:
-            cetak(LSegitiga(10,50));
-            break;
-            case 2:
-            cetak(VBalok(10,20,30));
-            break;
-            case 3:
-            cetak(VPrismaSegitiga(40,50,60));
-            break;
-            case 4:
-            cetak(VKerucut(50,70));
-            break;
-            default:
-            System.exit(0);
+        try{
+            Scanner input = new Scanner (System.in);
+            cetak ("=============================");
+            cetak ("SELAMAT DATANG DI PROGRAM MTK");
+            cetak ("1. Luas Segitiga");
+            cetak ("2. Volume Balok");
+            cetak ("3. Volume Prisma Segitiga");
+            cetak ("4. Volume Kerucut");
+            cetak ("5. Keluar");
+            cetak ("Silahkan pilih yang kamu mau : ");
+            int pilih = input.nextInt();
+            switch(pilih){
+                case 1:
+                baris ("alas = ");double a = input.nextDouble();
+                baris ("tinggi = ");double t = input.nextDouble();
+                cetak(LSegitiga(a,t));
+                break;
+                case 2:
+                baris ("panjang = "); double p = input.nextDouble();
+                baris ("lebar = "); double l = input.nextDouble();
+                baris ("tinggi = "); double t1 = input.nextDouble();
+                cetak(VBalok(p,l,t1));
+                break;
+                case 3:
+                baris ("panjang = "); double p1 = input.nextDouble();
+                baris ("lebar = "); double l1 = input.nextDouble();
+                baris ("tinggi = "); double t2 = input.nextDouble();
+                cetak(VPrismaSegitiga(p1,l1,t2));
+                break;
+                case 4:
+                baris ("jari-jari = "); double r = input.nextDouble();
+                baris ("tinggi = ");  double t3 = input.nextDouble();
+                cetak(VKerucut(r,t3));
+                break;
+                default:
+                System.exit(0);
+            }
+        }
+        catch (Exception e){
+            cetak("Data haruslah integer");
         }
     }
 }
